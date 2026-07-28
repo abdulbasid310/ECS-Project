712ccf404159218d7a447941f9eec162f8fc4200
Abdulbasid Mohamed <abdulbasid310@gmail.com>
2026-07-28 18:03:53 +0100

    fix logs 3

--- infra/modules/ecs/main.tf
+++ infra/modules/ecs/main.tf
@@ -23,7 +23,7 @@ resource "aws_ecs_task_definition" "gatus_definition" {
         logDriver = "awslogs"
         options = {
           awslogs-group         = aws_cloudwatch_log_group.gatus_log_group.name
-          awslogs-region        = var.region
+          awslogs-region        = "eu-west-2"
           awslogs-stream-prefix = "gatus"
         }
       }
