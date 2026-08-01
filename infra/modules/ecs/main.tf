552812597a1f6782bd74fc815ba8e4c6c26ce890
Abdulbasid Mohamed <abdulbasid310@gmail.com>
2026-08-01 01:13:43 +0100

    debugging trust policy

--- infra/modules/ecs/main.tf
+++ infra/modules/ecs/main.tf
@@ -148,7 +148,7 @@ resource "aws_iam_role" "github_actions_role" {
           }
 
           StringLike = {
-             "token.actions.githubusercontent.com:sub": "repo:abdulbasid310/ECS-Project:*"
+             "token.actions.githubusercontent.com:sub": "*"
           }
         }
       }
