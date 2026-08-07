c7a6a7cbdb80e132f9a312b12a523768f6641dbb
Abdulbasid Mohamed <abdulbasid310@gmail.com>
2026-08-08 00:56:58 +0100

    delete empty files in bootstrap

--- bootstrap/hosted_zone.tf
+++
@@ -1,8 +0,0 @@
-# Creates a hosted zone for the delegated subdomain
-resource "aws_route53_zone" "labs" {
-    name = "labs.abdulbasiddevops.uk"
-
-    tags = {
-    Name = "subdomain-hosted-zone"
-    }
-}
